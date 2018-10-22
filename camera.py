5d5ace946392c0e1a236b0ffdd9ce4a75f231053
mrx_mamun <almamunkhan04@gmail.com>
2018-10-22 23:32:41 +0600

    v-1.0 live streaming with flask

---
+++ camera.py
@@ -0,0 +1,33 @@
+import cv2
+
+cascPath = 'haarcascade_frontalface_dataset.xml'  # dataset
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+video_capture = cv2.VideoCapture(0)  # 0 for web camera live stream
+#  for cctv camera'rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp'
+#  example of cctv or rtsp: 'rtsp://mamun:123456@101.134.16.117:554/user=mamun_password=123456_channel=1_stream=0.sdp'
+
+
+
+
+def camera_stream():
+    while True:
+        # Capture frame-by-frame
+        ret, frame = video_capture.read()
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.CASCADE_SCALE_IMAGE
+        )
+
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+            # Display the resulting frame in browser
+            return cv2.imencode('.jpg', frame)[1].tobytes()
